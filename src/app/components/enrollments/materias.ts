edb0be73e95b7846a899dc2874d32154821bb333
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-02-24 23:48:13 -0400

    materias.ts inside enrollments component

---
+++ src/app/components/enrollments/materias.ts
@@ -0,0 +1,69 @@
+const usuarios = [
+    {
+        nombre: "Inteligencia Artifical",
+        sigla: "INF418",
+        docente: "Vargas Yapura Edwin",
+        grupo: "SA",
+        cupos: "20",
+        nivel: "7",
+        horario: "Mar 18:15-20:30 Jue 18:15-20:30"
+    },
+    {
+        nombre: "Investigacion Operativa II",
+        sigla: "MAT419",
+        docente: "Davalos Sanchez de Mancilla Pi",
+        grupo: "SB",
+        cupos: "20",
+        nivel: "7",
+        horario: "Mar 10:00-12:15 Jue 10:00-12:15"
+
+
+    },{
+        nombre: "Investigacion Operativa II",
+        sigla: "MAT419",
+        docente: "Sachez Herbas Jose Gabriel",
+        grupo: "SC",
+        cupos: "20",
+        nivel: "7",
+        horario: "LU 21:15-22:45 MIE 21:15-22:45 VIE 21:15-22:45"
+
+
+    },
+    { nombre: "Redes I", edad: 30 },
+    {
+        nombre: "Sistemas operativos II",
+        sigla: "INF413",
+        docente: "Sanchez Veleasco Enrique",
+        grupo: "SA",
+        cupos: "18",
+        nivel: "7",
+        horario: "LU 08:30-10:00 MIE 08:30-10:00 VIE 08:30-10:00"
+    },
+    {
+        nombre: "Sistemas operativos II",
+        sigla: "INF413",
+        docente: "Calle Terrazas Edwin",
+        grupo: "SB",
+        cupos: "20",
+        nivel: "7",
+        horario: "LU 13:00-15:15 JUE 20:30-22:45"
+    },
+    {
+        nombre: "Sistemas de Informacion II",
+        sigla: "INF412",
+        docente: "Martinez Canedo Rolando Antonio",
+        grupo: "SB",
+        cupos: "30",
+        nivel: "7",
+        horario: "LU 16:45-18:15 MIE 16:45-18:15 VIE 16:45-18:15"
+    },
+    {
+        nombre: "Sistemas de Informacion II",
+        sigla: "INF412",
+        docente: "Garzon Cuellar Angelica",
+        grupo: "SA",
+        cupos: "15",
+        nivel: "7",
+        horario: "Mar 07:00-09:15 Jue 07:00-09:15"
+    },
+];
